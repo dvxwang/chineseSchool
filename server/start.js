aaf3b433af097fd4d911c5d654f545f3c2e9500e
David <dvxwang@gmail.com>
2017-05-13 08:26:24 -0500

    Added pdf links

--- server/start.js
+++ server/start.js
@@ -6,8 +6,20 @@ var path = require('path');
 
 app.use(express.static('views'));
 
-app.get('/calendar', function(req, res, next) {
-	res.render('calendar');
+app.get('/link2', function(req, res, next) {
+	res.sendFile(path.join(__dirname, '../views/', 'ChineseSchool_Link2.pdf'));
+});
+
+app.get('/link3', function(req, res, next) {
+	res.sendFile(path.join(__dirname, '../views/', 'ChineseSchool_Link3.pdf'));
+});
+
+app.get('/link4', function(req, res, next) {
+	res.sendFile(path.join(__dirname, '../views/', 'ChineseSchool_Link4.pdf'));
+});
+
+app.get('/link6', function(req, res, next) {
+	res.sendFile(path.join(__dirname, '../views/', 'ChineseSchool_Link6.pdf'));
 });
 
 app.get('/*', function(req, res, next) {
